99e04e38f5fc84c5198141849602bb79812b5045
igorvelosocustodio@gmail.com <m0d4focA>
2017-12-04 00:19:19 -0200

    configuration docker

--- ui/public-html/app/common/common.module.js
+++ ui/public-html/app/common/common.module.js
@@ -4,7 +4,7 @@ angular.module('app.common', [])
 
     var returned= null;
     var getResponse = function(paymentInfo, callBack){
-        $http.post('http://localhost:8090/payment/buy', paymentInfo).
+        $http.post('/api/payment/buy', paymentInfo).
             then(function(respond){
                 callBack(true);
             }, function(respond) {
@@ -22,7 +22,7 @@ angular.module('app.common', [])
 
     var returned= null;
     var getResponse = function(id, callBack){
-        $http.put('http://localhost:8090/shopping/add/' + id).
+        $http.put('api/shopping/add/' + id).
             then(function(respond){
                 returned = 'Product added successfully!';
                 callBack(returned);
@@ -41,8 +41,8 @@ angular.module('app.common', [])
 
     var returned= null;
     var getResponse = function(callBack){
-//        $http.get('/poc-backend/product/list').
-        $http.get('http://localhost:8090/shopping/list').
+//        $http.get('http://localhost:8090/shopping/list').
+        $http.get('/api/shopping/list').
             then(function(respond){
                 returned = respond.data;
                 callBack(returned);
